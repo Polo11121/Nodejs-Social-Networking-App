58e71cc122adbafece70aa28067ab2e563e833be
michal.jasinski <michal.jasinski121@gmail.com>
2022-07-05 00:54:48 +0200

    Auth changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
-
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,6 +31,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -38,14 +39,13 @@ app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
 
-
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
+  console.log(req.cookies);
   next();
 });
 
