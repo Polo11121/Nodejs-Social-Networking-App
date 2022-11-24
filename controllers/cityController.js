d34effdfcf6ee838dbc68e9631c08bcd329433b7
Michał Jasiński <michal.jasinski11121.com>
2022-11-24 22:11:10 +0100

    Sort cities, add app url to send email function

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -5,7 +5,7 @@ const City = require('../models/cityModel');
 exports.getCities = catchAsync(async (req, res) => {
   const cities = await City.find({
     city: { $regex: req.params.name, $options: 'i' },
-  });
+  }).sort({ city: 1 });
 
   res.status(200).json({ status: 'success', data: cities });
 });
